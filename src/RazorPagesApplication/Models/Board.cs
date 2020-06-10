1d523ac25b90d4c0ef175686fb07d50dc8c74544
Adam Axelsson <adam.axelsson1992@gmail.com>
2020-06-10 20:03:41 +0200

    Added ViewBoardPage. Not finished, but works.

--- src/RazorPagesApplication/Models/Board.cs
+++ src/RazorPagesApplication/Models/Board.cs
@@ -10,5 +10,10 @@ namespace RazorPagesApplication.Models
         public long Id { get; private set; }
         public string Title { get; private set; }
         public List<Column> Columns { get; private set; }
+
+        public Board(long _id, string _title){
+            this.Id = _id;
+            this.Title = _title;
+        }
     }
 }
--- src/RazorPagesApplication/Pages/BoardPages/CreateBoard.cshtml.cs
+++ src/RazorPagesApplication/Pages/BoardPages/CreateBoard.cshtml.cs
@@ -7,34 +7,36 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using RazorPagesApplication.Models;
 
-namespace RazorPagesApplication.Pages
+namespace RazorPagesApplication.Pages.BoardPages
 {
     public class CreateBoardModel : PageModel
     {
         private readonly ILogger<CreateBoardModel> _logger;
 
-        public Board Board { get; set; }
-        public Column Column { get; set; }
-        public Item Item { get; set; }
-        public ItemDetails ItemDetails { get; set; }
+        private Board board { get; set; }
 
         public CreateBoardModel(ILogger<CreateBoardModel> logger)
         {
             _logger = logger;
         }
-
         public void OnGet()
         {
 
         }
         public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            //Send this item
             var boardTitle = Request.Form["create-board-title"];
-            Console.WriteLine(boardTitle);
             // TODO: Save to database
             //RazorPagesApplication.Models.Board board = await 
             //return RedirectToPage($"/Board/{board.Id}");
-            return RedirectToPage($"/Index");
+            board = new RazorPagesApplication.Models.Board(1, boardTitle);
+
+            return RedirectToPage($"/BoardPages/ViewBoard", board);
         }
     }
 }
