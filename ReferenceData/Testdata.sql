c28ada6b9cd72279da39f2e321ac27a53bd0e931
Lucas Wiseby <LucasW@centiro.se>
2020-06-10 15:39:06 +0200

    Insert Test Data Script

--- ReferenceData/Testdata.sql
+++ ReferenceData/Testdata.sql
@@ -5,13 +5,23 @@ insert into Columns(Id, Title, BoardId) values (1, "Test 1", 1 );
 insert into Columns(Id, Title, BoardId) values (2, "Test 2", 1 );
 insert into Columns(Id, Title, BoardId) values (3, "Test 3", 1 );
 
-insert into Columns(Id, Title, BoardId) values (1, "Test 1", 2 );
-insert into Columns(Id, Title, BoardId) values (2, "Test 2", 2 );
-insert into Columns(Id, Title, BoardId) values (3, "Test 3", 2 );
-insert into Columns(Id, Title, BoardId) values (4, "Test 4", 2 );
-insert into Columns(Id, Title, BoardId) values (5, "Test 5", 2 );
+insert into Columns(Id, Title, BoardId) values (4, "Test 1", 2 );
+insert into Columns(Id, Title, BoardId) values (5, "Test 2", 2 );
+insert into Columns(Id, Title, BoardId) values (6, "Test 3", 2 );
+insert into Columns(Id, Title, BoardId) values (7, "Test 4", 2 );
+insert into Columns(Id, Title, BoardId) values (8, "Test 5", 2 );
 
-insert into Items (Id, Title, Description, BoardId) values(1, "Gå ut med hunden", "Efter jobbet måste detta ske", 1);
-insert into Items (Id, Title, Description, BoardId) values(2, "Hämta mat", "Två portioner", 1);
-insert into Items (Id, Title, Description, BoardId) values(3, "Städa toa", "Använd grönsåpa", 1);
-insert into Items (Id, Title, Description, BoardId) values(4, "Besiktiga bilen", "Föra april", 1);
+insert into Items (Title, Description, ColumnId) values("Gå ut med hunden", "Efter jobbet måste detta ske", 1);
+insert into Items (Title, Description, ColumnId) values("Hämta mat", "Två portioner", 1);
+insert into Items (Title, Description, ColumnId) values("Städa toa", "Använd grönsåpa", 1);
+insert into Items (Title, Description, ColumnId) values("Besiktiga bilen", "Föra april", 1);
+
+
+insert into Items (Title, Description, ColumnId) values("Gå ut med hunden", "Efter jobbet måste detta ske", 2);
+insert into Items (Title, Description, ColumnId) values("Hämta mat", "Två portioner", 2);
+insert into Items (Title, Description, ColumnId) values("Städa toa", "Använd grönsåpa", 2);
+insert into Items (Title, Description, ColumnId) values("Besiktiga bilen", "Föra april", 2);
+insert into Items (Title, Description, ColumnId) values("Gå ut med hunden", "Efter jobbet måste detta ske", 2);
+insert into Items (Title, Description, ColumnId) values("Hämta mat", "Två portioner", 2);
+insert into Items (Title, Description, ColumnId) values("Städa toa", "Använd grönsåpa", 2);
+insert into Items (Title, Description, ColumnId) values("Besiktiga bilen", "Föra april", 2);
