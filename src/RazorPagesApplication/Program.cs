e87ee58bfbe7d23b4181cd39de88900b98391338
vovve <vovve73@icloud.com>
2020-06-11 14:52:42 +0200

    Removed folderadding in main

--- src/RazorPagesApplication/Program.cs
+++ src/RazorPagesApplication/Program.cs
@@ -16,31 +16,6 @@ namespace RazorPagesApplication
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-
-            // Specify the directory you want to manipulate.
-            string path = @"~\data";
-
-            try
-            {
-                // Determine whether the directory exists.
-                if (Directory.Exists(path))
-                {
-                    Console.WriteLine("That path exists already.");
-                    return;
-                }
-
-                // Try to create the directory.
-                DirectoryInfo di = Directory.CreateDirectory(path);
-                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
-
-                // Delete the directory.
-                di.Delete();
-                Console.WriteLine("The directory was deleted successfully.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The process failed: {0}", e.ToString());
-            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
